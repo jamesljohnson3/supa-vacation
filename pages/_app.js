96e9420050dc7b4cb9bdaa4897254495cb57d8f4
Admin <support@unlimitpotential.com>
2023-01-03 22:18:39 -0500

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider as AuthProvider } from 'next-auth/react';
+import {ClerkProvider, RedirectToSignIn, SignedIn, SignedOut,} from "@clerk/nextjs";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <AuthProvider session={session}>
-        <Component {...pageProps} />
+      <ClerkProvider {...pageProps}></ClerkProvider>
       </AuthProvider>
 
       <Toaster />
