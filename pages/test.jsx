38bc51c4a3685780ac733bc549d71c8c4f415b71
Admin <support@unlimitpotential.com>
2023-01-05 00:48:04 -0500

    k

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,5 +1,5 @@
 import { useClerk } from '@clerk/clerk-react';
-import { getSession } from 'next-auth/client';
+import { getSession } from 'next-auth/react';
 
 function MyComponent() {
   const { isLoaded, session } = useClerk();
