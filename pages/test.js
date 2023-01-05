cbe1f564155de2e6257033e481d6adc10aa62573
Admin <support@unlimitpotential.com>
2023-01-05 13:14:03 -0500

    a

---
+++ pages/test.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { useClerk } from 'clerk';
+import { NextAuthContext } from 'next-auth/client';
+
+const MyComponent = () => {
+  const { user, loading } = useClerk();
+  const { setUser } = useContext(NextAuthContext);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (user) {
+    setUser(user);
+  }
+
+  return <p>User data: {JSON.stringify(user)}</p>;
+}
