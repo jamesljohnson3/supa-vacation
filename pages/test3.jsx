13e7cd7b3ea57a56ebbb6b2e8978672181fa8ebc
Admin <support@unlimitpotential.com>
2023-01-05 02:09:31 -0500

    a

---
+++ pages/test3.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const EmailForm = () => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // Send API call to NextAuth to trigger email API
+      await axios.post('/api/auth/email/send', {
+        email,
+      });
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Send Email'}
+      </button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
+
+export default EmailForm;
