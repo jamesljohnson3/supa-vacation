6d46f0cd872af87e6597a9fc33ebcc64121f2e81
Admin <support@unlimitpotential.com>
2023-01-05 01:06:08 -0500

    a

--- pages/test.jsx
+++ pages/test.jsx
@@ -16,7 +16,7 @@ function MyComponent() {
     return <div>Not authenticated</div>;
   }
 
-  return  <div>Session: {JSON.stringify(session)}</div>;
+  return <div>Authenticated</div>;
 }
 
 export default MyComponent
